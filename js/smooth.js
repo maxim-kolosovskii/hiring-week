4bd1ede2929c4a4cd594ac568dc82944e60a6475
maxim-kolosovskii <66016998+maxim-kolosovskii@users.noreply.github.com>
2022-01-19 16:31:14 +0300

    #31945 updated

--- js/smooth.js
+++ js/smooth.js
@@ -20,3 +20,30 @@ focusMethod = function getFocus() {
 //     document.getElementById("form-fio").focus();
 // }
 
+$(function() {
+    // Stick the #fillJobForm to the top of the window
+    var nav = $('#fillJobForm');
+    var navHomeY = nav.offset().top;
+    var isFixed = false;
+    var $w = $(window);
+    $w.scroll(function() {
+        var scrollTop = $w.scrollTop();
+        var shouldBeFixed = scrollTop > navHomeY;
+        if (shouldBeFixed && !isFixed) {
+            nav.css({
+                position: 'fixed',
+                top: 0,
+                left: nav.offset().left,
+                width: nav.width()
+            });
+            isFixed = true;
+        }
+        else if (!shouldBeFixed && isFixed)
+        {
+            nav.css({
+                position: 'static'
+            });
+            isFixed = false;
+        }
+    });
+});
